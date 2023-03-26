38775dee67d12f1ba72621629a31f9717755c009
zuhalal <zuhalalimulhadi19@gmail.com>
2023-03-26 14:40:33 +0700

    refactor: remove unused sample data

--- app/src/main/java/com/example/githubusers/data/sample/SampleGithubUserData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.githubusers.data.sample
-
-import com.example.githubusers.data.remote.models.UserResponseItem
-
-class SampleGithubUserData {
-    fun getUserResponseData(): Array<UserResponseItem> {
-        return SampleUserResponseData.data
-    }
-}
